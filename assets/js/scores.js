0bc333b6a88f3062bbcea10e07e5be0010250580
Jodi Jackson <jjacksonjayhawkfan@gmail.com>
2021-07-18 21:20:15 -0500

    start  local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,6 +2,12 @@ const username = document.getElementById ('username');
 const saveScoreButton = document.getElementById('saveScoreButton');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScoresList = document.getElementById("highScoresList");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const max_high_scores = 5;
+
 finalScoreinnterText = mostRecentScore;
 
 username.addEventListener ('keyup', () =>{
@@ -10,4 +16,23 @@ username.addEventListener ('keyup', () =>{
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+
+    };
+    highScores.push(score)
+    highScores.sort ( (a,b) => b.score-a.score);
+    highScores.splice (5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
+
+
+highScoresList.innerHTML = highScores
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
